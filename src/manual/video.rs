e053264d7b3249018bb7aa6bda4f7dc4a6470d6f
Alessandro Chitolina <alekitto@gmail.com>
2021-10-04 18:14:20 +0200

    implement Send for Ptr<TrackerCSRT>

--- src/manual/video.rs
+++ src/manual/video.rs
@@ -1,2 +1,3 @@
+unsafe impl Send for crate::core::Ptr<dyn crate::tracking::TrackerCSRT> {}
 unsafe impl Send for crate::core::Ptr<dyn crate::tracking::TrackerKCF> {}
 unsafe impl Send for crate::core::Ptr<dyn crate::video::TrackerGOTURN> {}
